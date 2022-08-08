3c3583953fc5337a58a3426aa6798ecf68bf6f69
yoav <yoavdan.m@gmail.com>
2022-08-08 21:55:21 +0300

    sign-up page css done

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -6,7 +6,7 @@ const Signup = () => {
     return (
         <section className="sign-up-page  bg-gradient-to-b from-sky-900 to-zinc-300   ">
             <main className='sign-up-card rounded-md'   >
-                <button className='sign-with-google rounded-lg border-2 text-slate-50 '><span><GoogleIcon /></span> Sign In with Google</button>
+                <button className='sign-with-google rounded-3xl border-2 text-slate-50 '><span><GoogleIcon /></span> Sign In with Google</button>
                 <form>
                     <div className="form-item">
                         <label htmlFor="fullname">Full Name</label>
